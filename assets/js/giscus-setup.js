72660817169ec89a1949d840e67a6271724dede8
JeongSeoJin <71913312+JeongSeoJin@users.noreply.github.com>
2026-01-22 09:14:54 +0000

    Deploying to gh-pages from @ JeongSeoJin/JeongSeoJin.github.io@9ef5adb2e02d02b23dc4cc8c2f2b40c0a3651228 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -19,9 +19,9 @@ function determineGiscusTheme() {
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
     "data-repo": "JeongSeoJin/JeongSeoJin.github.io",
-    "data-repo-id": "",
+    "data-repo-id": "R_kgDOQ8suog",
     "data-category": "Comments",
-    "data-category-id": "",
+    "data-category-id": "DIC_kwDOQ8suos4C1RcP",
     "data-mapping": "title",
     "data-strict": "1",
     "data-reactions-enabled": "1",
